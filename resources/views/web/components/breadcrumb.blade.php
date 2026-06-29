439467b374f6e35292de98bafdc828d26a99d2aa
syevolution <syevolution.web@gmail.com>
2026-06-28 23:21:21 -0500

    breadscrumbs

--- resources/views/web/components/breadcrumb.blade.php
+++ resources/views/web/components/breadcrumb.blade.php
@@ -1,14 +1,14 @@
 <!-- Breadcrumb -->
-<nav {{ $attributes ?? '' }}>
+<nav class="breadcrumb" {{ $attributes ?? '' }}>
     @foreach($items ?? [] as $index => $item)
         @if($item['url'] ?? false)
-            <a href="{{ $item['url'] }}" {{ $item['linkAttributes'] ?? '' }}>{{ $item['label'] }}</a>
+            <a href="{{ $item['url'] }}" class="breadcrumb-link" {{ $item['linkAttributes'] ?? '' }}>{{ $item['label'] }}</a>
         @else
-            <span {{ $item['spanAttributes'] ?? '' }}>{{ $item['label'] }}</span>
+            <span class="breadcrumb-active" {{ $item['spanAttributes'] ?? '' }}>{{ $item['label'] }}</span>
         @endif
 
         @if($index < count($items) - 1)
-            <span {{ $separatorAttributes ?? '' }}>{{ $separator ?? '>' }}</span>
+            <span class="breadcrumb-separator" {{ $separatorAttributes ?? '' }}>{{ $separator ?? '>' }}</span>
         @endif
     @endforeach
 </nav>
